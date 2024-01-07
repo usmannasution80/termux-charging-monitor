59e3211fdbb50c369215a9de4440b049579d59cb
Usman Nasution <usmannasty32@gmail.com>
2024-01-07 23:56:42 +0700

    Add main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import json
+import time
+
+status = None
+
+while True:
+  temp_path = os.path.dirname(os.path.realpath(__file__)) + '/temp'
+  battery_status_prompt = 'termux-battery-status > ' + temp_path
+
+  os.system(battery_status_prompt)
+
+  temp_file = open(temp_path)
+  battery_status = json.loads(temp_file.read())
+  temp_file.close()
+
+  if status == None:
+    status = battery_status['status']
+    time.sleep(1)
+    continue
+
+  if status != battery_status['status']:
+    status = battery_status['status']
+    os.system('termux-notification -c ' + status)
