a81c5f09e0e3ebd1eade48b572d272a8980ef663
Usman Nasution <usmannasty32@gmail.com>
2024-01-25 23:36:25 +0700

    Some changes

--- ValueInvalidError.py
+++ ValueInvalidError.py
@@ -1,3 +1,4 @@
 class ValueInvalidError(Exception):
-  def __init__(self, message):
-    super().__init__(message)
+  def __init__(self, message = ''):
+    self.message = message
+    super().__init__(self.message)
